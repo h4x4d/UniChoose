215bfe46f831d7562eeedf41892e592d7a4c9223
h4x4d <cleanyboom@gmail.com>
2022-12-07 21:54:28 +0300

    docker media | static

--- UniChoose/UniChoose/urls.py
+++ UniChoose/UniChoose/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
+
+from UniChoose.UniChoose import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
