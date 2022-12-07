a4d841683b5ddad937e01cb9989be86bfd8845c9
h4x4d <cleanyboom@gmail.com>
2022-12-07 18:59:12 +0300

    CI/CD | flake8 pulgins update

--- UniChoose/UniChoose/settings.py
+++ UniChoose/UniChoose/settings.py
@@ -1,8 +1,7 @@
 import os
 from pathlib import Path
 
-
-if os.getenv("DEVELOP", 0):
+if os.getenv('DEVELOP', 0):
     from dotenv import load_dotenv
 
     load_dotenv('../.env')
@@ -10,11 +9,11 @@ if os.getenv("DEVELOP", 0):
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,14 +55,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'UniChoose.wsgi.application'
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
