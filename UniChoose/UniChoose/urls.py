267bd7cbba6b6f46bdcbf0d84a76d0e60920db2c
h4x4d <cleanyboom@gmail.com>
2022-12-08 21:09:05 +0300

    docker static test

--- UniChoose/UniChoose/urls.py
+++ UniChoose/UniChoose/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.urls import path
-
-from UniChoose import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
