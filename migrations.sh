d99ff664ab603a03b24aaac05aefd4907dff4a4d
Сиденко Олег <86770740+h4x4d@users.noreply.github.com>
2022-12-08 21:27:28 +0300

    Docker static (#3)
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | append_slash
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * static in DEBUG
    
    * delete useless linter
    
    * delete useless linter

--- migrations.sh
+++ migrations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Waiting for postgres..."
+python UniChoose/manage.py collectstatic
 
 while ! nc -z $SQL_HOST $SQL_PORT; do
   sleep 0.1
@@ -8,6 +9,6 @@ done
 
 echo "PostgreSQL started"
 
-python manage.py migrate
+python UniChoose/manage.py migrate
 
 exec "$@"
