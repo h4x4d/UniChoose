f5f04caa492a0e8852d5c8c06fafd83ab60190c4
Сиденко Олег <86770740+h4x4d@users.noreply.github.com>
2022-12-08 21:27:28 +0300

    Docker static (#3)
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | static
    
    * docker media | append_slash
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * docker static test
    
    * static in DEBUG
    
    * delete useless linter
    
    * delete useless linter

--- UniChoose/UniChoose/settings.py
+++ UniChoose/UniChoose/settings.py
@@ -6,7 +6,6 @@ if os.getenv('DEVELOP', 0):
 
     load_dotenv('../.env')
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -95,5 +94,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = True
