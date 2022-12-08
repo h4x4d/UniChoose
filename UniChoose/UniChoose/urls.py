150b8bacefdab4d502c2f9737df55dfe55540023
h4x4d <cleanyboom@gmail.com>
2022-12-08 21:20:52 +0300

    static in DEBUG

--- UniChoose/UniChoose/urls.py
+++ UniChoose/UniChoose/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
+from UniChoose import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
