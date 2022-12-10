6a14bf3ef5367ec8ddc1511eb73599298a3ba4b6
ChavronV <97131681+WhatWouldDarknessSay@users.noreply.github.com>
2022-12-10 20:16:45 +0300

    markup header, footer and homepage
    No variability due to no user system, no pages, etc.

--- UniChoose/UniChoose/urls.py
+++ UniChoose/UniChoose/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from UniChoose import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
 ]
 
 if settings.DEBUG:
