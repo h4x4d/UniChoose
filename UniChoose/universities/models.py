6cc9614e5bbbe8473260509bace20d4da42a4fcb
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2022-12-10 22:31:34 +0300

    fixed migrations and linting

--- UniChoose/universities/models.py
+++ UniChoose/universities/models.py
@@ -1,21 +1,25 @@
-from core.models import CoreNameModel
 from django.core.validators import MaxValueValidator
 from django.db import models
 
+from core.models import CoreNameModel
+
 
 class University(CoreNameModel):
-    description = models.TextField(verbose_name='description',
-                                   blank=True, null=True)
-    avg_rating = models.DecimalField(verbose_name='average rating',
-                                     max_digits=3, decimal_places=1,
-                                     validators=(MaxValueValidator(10.0),),
-                                     )
+    description = models.TextField(
+        verbose_name='description', blank=True, null=True)
+    avg_rating = models.DecimalField(
+        verbose_name='average rating',
+        max_digits=3,
+        decimal_places=1,
+        validators=(MaxValueValidator(10.0),),
+    )
     rating_count = models.PositiveSmallIntegerField(
         verbose_name='rating count',
     )
-    region = models.CharField(verbose_name='region',
-                              max_length=100,
-                              )
+    region = models.CharField(
+        verbose_name='region',
+        max_length=100,
+    )
 
     def __str__(self):
         return self.name
