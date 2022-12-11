268ba544c76442423ebb302cdf4f874fa37cb4b7
Maxim Sharkevich <sharkevich.maxim@gmail.com>
2022-12-11 11:06:43 +0300

    Basic views done, further development required (#7)
    * created basic views for...
    ...
    - homepage
    - liked universities list
    - liked departments list
    (!) Last two are not tested (!)
    
    - added `liked_departments` and `liked_universities` fields to account model
    - moved `AccountDepartmentRelation` model to `users` app to avoid circular import
    - made migrations
    - marked up some template directories and demo views
    - added some comments and TODOs
    
    * linting fixed
    
    * models fixes
    
    Co-authored-by: h4x4d <cleanyboom@gmail.com>

---
+++ UniChoose/departments/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from users.models import Account
+
+# TODO: rewrite view according to Alexey templates
+
+
+class LikedDepartments(ListView):
+    # ! Haven't tested this view yet
+
+    template_name = 'departments/index.html'
+
+    def get_queryset(self, account: Account):
+        return account.liked_dpts.all()
+
+    def get(self, request):
+        context = {
+            'liked_departments': self.get_queryset(request.user),
+        }
+
+        render(request, self.template_name, context)
