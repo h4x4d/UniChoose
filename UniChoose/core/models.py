a3754150e63ad178201b1f72b178330b4e09e4f6
Сиденко Олег <86770740+h4x4d@users.noreply.github.com>
2022-12-12 21:21:19 +0300

    Models (#13)
    * university filters
    
    * university filters
    
    * Models changes
    
    * Models changes

--- UniChoose/core/models.py
+++ UniChoose/core/models.py
@@ -1,8 +1,26 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 
 class CoreNameModel(models.Model):
     name = models.CharField(verbose_name='name', max_length=200)
+
+    class Meta:
+        abstract = True
+
+
+class WeightedModel(models.Model):
+    entry_score = models.IntegerField(blank=True, null=True)
+    vuz_rating = models.DecimalField(
+        verbose_name='average rating',
+        max_digits=3,
+        decimal_places=1,
+        validators=(MaxValueValidator(10.0), ),
+        blank=True,
+        null=True,
+    )
+    edu_level = models.IntegerField(blank=True, null=True)
+    profile = models.IntegerField(blank=True, null=True)
 
     class Meta:
         abstract = True
