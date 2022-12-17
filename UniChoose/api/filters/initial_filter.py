70ebcbb69771fdf89f18b0f7f37073bea1b87c8b
h4x4d <cleanyboom@gmail.com>
2022-12-17 14:25:12 +0300

    code fixes

--- UniChoose/api/filters/initial_filter.py
+++ UniChoose/api/filters/initial_filter.py
@@ -2,8 +2,6 @@ from api.filters.distance_filter import distance_checker
 from api.filters.subjects_filter import departments_checker
 from departments.models import Department
 from universities.models import Region, University
-from users.models import AccountDepartmentRelations
-from django.db.models import Prefetch
 
 
 def initial_filter(user):
@@ -18,27 +16,14 @@ def initial_filter(user):
     user_id = user.id
 
     departments = []
-    # department_queryset = (Department.objects
-    #                        .filter(university__in=universities)
-    #                        .order_by('entry_score', 'university__avg_rating')
-    #                        .select_related('relations'))
 
-    # queryset = (AccountDepartmentRelations.objects.select_related('department',
-    #                                                               'account')
-    #             .filter(department__university__in=universities)
-    #             .order_by('department__entry_score',
-    #                       'department__university__avg_rating'))
-
-    queryset = (Department.objects
-                .prefetch_related('relations')
-                .filter(university__in=universities)
-                .order_by('entry_score', 'university__avg_rating'))
-
-    print(len(queryset))
+    queryset = (Department.objects.prefetch_related('relations').filter(
+        university__in=universities).order_by('entry_score',
+                                              'university__avg_rating'))
 
     for department in queryset:
-        print(department)
-        if departments_checker(queryset, user_subjects, user_marks, user_id, department):
+        if departments_checker(queryset, user_subjects, user_marks, user_id,
+                               department):
             departments.append(department)
 
     return departments
